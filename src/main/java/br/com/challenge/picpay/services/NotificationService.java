c6f59f74b5884a73e7c494296d8a98e19d321131
luanfmendes <lna_mendes@hotmail.com>
2023-09-16 16:29:15 -0300

    feat: add validation of notificaiton microservice is availabe

--- src/main/java/br/com/challenge/picpay/services/NotificationService.java
+++ src/main/java/br/com/challenge/picpay/services/NotificationService.java
@@ -8,11 +8,28 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class NotificationService {
+    //private static final String NOTIFICATION_URL = "http://o4d9z.mocklab.io/notify";
     @Autowired
     private RestTemplate restTemplate;
 
     public void sendNotification(User user, String message) throws Exception {
         String email = user.getEmail();
         NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+        /*HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<NotificationDTO> requestEntity = new HttpEntity<>(notificationRequest, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                NOTIFICATION_URL,
+                HttpMethod.POST,
+                requestEntity,
+                String.class
+        );
+
+        if (response.getStatusCode() != HttpStatus.OK) {
+            throw new Exception("Serviço de notificação está fora do ar");
+        }*/
     }
 }
